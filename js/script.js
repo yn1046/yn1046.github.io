0737684077d1e3c6484c068c7558eca1201cf597
yn1046 <wantedblood@2ch.hk>
2018-02-17 00:33:21 +0300

    Scroll-based style implemented

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+let kek = document.getElementById('msg');
+let fizz = document.getElementById('fizz');
+
 window.addEventListener('scroll', function(e) {
-    document.getElementById('fizz').style.backgroundColor='lightblue';
+    let offset = window.pageYOffset;
+    kek.innerHTML = `Offset: ${offset}`;
+    if (offset === 0) fizz.style.backgroundColor = 'crimson';
+    else fizz.style.backgroundColor = 'lightblue';
 });
